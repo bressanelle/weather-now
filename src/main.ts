38afa8d5f3a19bf161fc252ce470f46e0d29c75d
Hiury <contatobressanelli@gmail.com>
2021-06-22 04:36:37 -0300

    chore(customElements): allow @deprecated/weather-now-component-library

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { defineCustomElements } from '@deprecat3d/weather-now-component-library';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+defineCustomElements();
