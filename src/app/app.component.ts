a741c75994d5bd774307eb54f0f95a8502c8ec90
Hiury <contatobressanelli@gmail.com>
2021-06-23 01:25:29 -0300

    chore: remove default favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import '@deprecat3d/weather-now-component-library';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'weather-now';
